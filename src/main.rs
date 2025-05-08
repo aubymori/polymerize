d31f07ec704e83ed6c0692bc46339ceb10f02ff4
aubymori <aubyomori@gmail.com>
2025-05-08 07:37:35 -0500

    Move to Actix

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-#[macro_use] extern crate rocket;
-use rocket::response::content::RawHtml;
+use actix_web::{get, web, App, HttpResponse, HttpRequest, HttpServer, Responder}; 
 use askama::Template;
 mod polymer;
 
 #[get("/")]
-fn home() -> RawHtml<String> {
+async fn home() -> impl Responder {
     let tmpl = polymer::CoreTemplate { config: "{\"test\":\"test\"}", initial_data: "shit" };
-    RawHtml(tmpl.render().unwrap())
+    HttpResponse::Ok().body(tmpl.render().unwrap())
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount(
-        "/", routes![home]
-    )
+async fn default(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body("default")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+            .default_service(web::get().to(default))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
